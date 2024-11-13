beaf6c347daa83f2fa3a951960182cbba1efcbf0
Shuudy <ancieux.jerome059@gmail.com>
2024-11-13 16:22:07 +0100

    feat(hero): add hero component with styles in _hero.scss

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,15 @@
+<div class="hero">
+    <div class="hero-content">
+
+        <div class="hero-left">
+            <h1>ARNGREN,</h1>
+            <h2>Une solution pour vos <span>besoins</span></h2>
+
+            <a href="#" class="button">Acheter maintenant</a>
+        </div>
+
+        <div class="hero-right">
+            <img src="https://ajilink.fr/wp-content/uploads/2024/04/CRT-TRANSPORT-ID-logistics-prevention-entreprise-en-difficulte-restructuring-administrateur-judiciaire-AJILINK.jpeg" alt="">
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('body')
-
+<x-hero/>
 @endsection

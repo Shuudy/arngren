86d613fafd9dee5f345b9d0187820b059f02891a
Shuudy <ancieux.jerome059@gmail.com>
2024-11-13 20:58:27 +0100

    feat(product): add style, view, and route for single product page

---
+++ resources/views/product.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('body')
+<x-navbar/>
+
+<div class="page-main">    
+
+    <div class="flex">     
+
+        <div class="productd-image">
+            <div class="productd-image-big">
+                <img src="https://www.kompressor-bike.com/wp-content/uploads/2020/09/velo-vtt.png" alt="">
+            </div>
+            <div class="productd-image-smalls">
+                @for($i = 0; $i < 4; $i++)
+                <div class="productd-image-small">
+                    <img src="https://www.kompressor-bike.com/wp-content/uploads/2020/09/velo-vtt.png" alt="">
+                </div>
+                @endfor
+            </div>
+        </div>
+
+        <div class="productd-infos">
+            
+            <div class="productd-title">Un produit de test</div>
+            <div class="productd-price">24.99 €</div>   
+
+            <a href="#" class="button">Acheter l'article</a>
+
+            <div class="productd-description">
+                Un super produit de test
+            </div>
+
+        </div>
+
+    </div>
+    
+</div>
+<x-footer/>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');
 
 Route::view('/contact', 'contact')->name('contact');
+Route::view('/products/1', 'product')->name('product');
 Route::view('/products', 'products')->name('products');

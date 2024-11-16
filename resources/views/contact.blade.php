24648defbd635e5799bc2634ac95bb66b3fcab86
Shuudy <ancieux.jerome059@gmail.com>
2024-11-16 20:36:13 +0100

    feat(contact): update maps iframe with correct address

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,7 @@
 
 <div class="contact">
     <div class="contact-maps">
-        <div style="width: 100%"><iframe width="100%" height="150" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=8&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe></div>
+        <div style="width: 100%"><iframe width="100%" height="150" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=57%20Rue%20Pierre%20Mauroy,%2059800%20Lille&amp;t=&amp;z=8&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe></div>
     </div>
     <div class="contact-content">
         <div class="contact-left">
